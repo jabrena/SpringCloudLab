3683abeb8bf8f5b8d7ee6a0e5d27d6959ed78eb2
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-02-28 13:11:21 +0100

    https://github.com/jabrena/SpringCloudLab/issues/18

--- SpringBoot/Example1/src/main/java/io/pivotal/cf/example/SpringBootStarter.java
+++ SpringBoot/Example1/src/main/java/io/pivotal/cf/example/SpringBootStarter.java
@@ -1,11 +1,9 @@
 package io.pivotal.cf.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class SpringBootStarter {
 
     public static void main(String[] args) {
--- SpringBoot/Example1/src/main/java/io/pivotal/cf/example/controllers/HelloWorldController.java
+++ SpringBoot/Example1/src/main/java/io/pivotal/cf/example/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package io.pivotal.cf.example;
+package io.pivotal.cf.example.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
